b711946166ce3a9edc75ea1c8ac15684087e5278
zer0 <osom8979@gmail.com>
2024-08-13 15:37:36 +0900

    Implement CudaHandler

--- smipc/cuda/handler.py
+++ smipc/cuda/handler.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from functools import reduce
 from typing import Optional, Sequence
 
 import numpy
@@ -26,11 +27,11 @@ def get_device_properties(device_index: int):
     return cupy.cuda.runtime.getDeviceProperties(device_index)
 
 
-def ipc_get_mem_handle(device_ptr: int):
+def ipc_get_mem_handle(device_ptr: int) -> bytes:
     return cupy.cuda.runtime.ipcGetMemHandle(device_ptr)
 
 
-def ipc_open_mem_handle(handle: bytes, flags: Optional[int] = None):
+def ipc_open_mem_handle(handle: bytes, flags: Optional[int] = None) -> int:
     if flags is None:
         flags = cupy.cuda.runtime.cudaIpcMemLazyEnablePeerAccess
     assert isinstance(flags, int)
@@ -41,23 +42,70 @@ def ipc_close_mem_handle(device_ptr: int):
     return cupy.cuda.runtime.ipcCloseMemHandle(device_ptr)
 
 
-def ipc_get_event_handle(event: int):
+def ipc_get_event_handle(event: int) -> bytes:
     return cupy.cuda.runtime.ipcGetEventHandle(event)
 
 
-def ipc_open_event_handle(handle: bytes):
+def ipc_open_event_handle(handle: bytes) -> int:
     return cupy.cuda.runtime.ipcOpenEventHandle(handle)
 
 
-class CudaHandler:
-    _mem: Optional[CudaMemory]
+def stream_wait_event(stream: int, event: int, flags=0):
+    # cudaEventWaitDefault == 0
+    # cudaEventWaitExternal == 1
+    return cupy.cuda.runtime.streamWaitEvent(stream, event, flags)
 
-    def __init__(self, size: int, device=None):
+
+def memcpy_async(dst: int, src: int, size: int, kind: int, stream: int):
+    return cupy.cuda.runtime.memcpyAsync(dst, src, size, kind, stream)
+
+
+def memcpy_async_host_to_device(dst: int, src: int, size: int, stream: int):
+    assert cupy.cuda.runtime.memcpyHostToDevice == 1
+    return memcpy_async(dst, src, size, cupy.cuda.runtime.memcpyHostToDevice, stream)
+
+
+def memcpy_async_device_to_host(dst: int, src: int, size: int, stream: int):
+    assert cupy.cuda.runtime.memcpyDeviceToHost == 2
+    return memcpy_async(dst, src, size, cupy.cuda.runtime.memcpyDeviceToHost, stream)
+
+
+class IpcReceiver:
+    def __init__(self, mem: CudaMemory):
+        self._shape = mem.shape
+        self._memory_size = mem.memory_size
+        self._stride = mem.stride
+
+        self._device_ptr = ipc_open_mem_handle(mem.memory_handle)
+        self._um = cupy.cuda.UnownedMemory(self._device_ptr, mem.memory_size, 0)
+        self._mp = cupy.cuda.MemoryPointer(self._um, 0)
+
+        self._device = cupy.cuda.Device(device=mem.device_index)
+        self._event_ptr = ipc_open_event_handle(mem.event_handle)
+        self._stream = cupy.cuda.get_current_stream(mem.device_index)
+
+    def close(self):
+        ipc_close_mem_handle(self._device_ptr)
+
+    def record(self):
+        cupy.cuda.runtime.eventRecord(self._event_ptr, self._stream.ptr)
+
+    def wait(self):
+        stream_wait_event(self._stream.ptr, self._event_ptr)
+
+    def as_gpu(self, dtype):
+        return cupy.ndarray(self._shape, dtype=dtype, memptr=self._mp)
+
+
+class CudaHandler:
+    def __init__(self, shape: Sequence[int], dtype, stride=0, device=None):
+        size = reduce(lambda x, y: x * y, shape, 1)
+
         if size <= 0:
             raise ValueError("size must be positive")
 
-        self._mem = None
         self._size = align_ipc_malloc_size(size)
+        self._stride = stride
 
         assert self._size >= IPC_ALLOCATION_UNIT_SIZE
         assert self._size % IPC_ALLOCATION_UNIT_SIZE == 0
@@ -70,8 +118,30 @@ class CudaHandler:
         )
 
         with self._device:
-            self._cpu = self.cpu_memory_pool().malloc(self._size)
-            self._gpu = self.gpu_memory_pool().malloc(self._size)
+            self._cpu_memory = self.cpu_memory_pool().malloc(self._size)
+            self._gpu_memory = self.gpu_memory_pool().malloc(self._size)
+
+        self._cpu = numpy.ndarray(shape=shape, dtype=dtype, buffer=self._cpu_memory)  # type: ignore[var-annotated] # noqa E501
+        self._gpu = cupy.ndarray(shape=shape, dtype=dtype, memptr=self._gpu_memory)
+
+        self._event = cupy.cuda.Event(
+            block=False,
+            disable_timing=True,
+            interprocess=True,
+        )
+        self._stream = cupy.cuda.Stream(non_blocking=True)
+
+        self._event_handle = ipc_get_event_handle(self._event.ptr)
+        self._memory_handle = ipc_get_mem_handle(self._gpu.data.ptr)
+
+        self._mem = CudaMemory(
+            self._device.id,
+            self._event_handle,
+            self._memory_handle,
+            self._size,
+            self._stride,
+            self._cpu.shape,
+        )
 
     @staticmethod
     def cpu_memory_pool():
@@ -90,26 +160,45 @@ class CudaHandler:
         return f"cuda:{self._device.id}"
 
     @property
+    def cpu(self):
+        return self._cpu
+
+    @property
+    def gpu(self):
+        return self._gpu
+
+    @property
     def size(self):
         return self._size
 
-    def as_numpy(self, shape: Sequence[int], dtype):
-        return numpy.ndarray(shape=shape, dtype=dtype, buffer=self._cpu)
+    @property
+    def stride(self):
+        return self._stride
 
-    def as_cupy(self, shape: Sequence[int], dtype):
-        return cupy.ndarray(shape=shape, dtype=dtype, memptr=self._gpu)
+    def as_cpu_tensor(self):
+        return torch.from_numpy(self._cpu)
 
-    def as_cpu_tensor(self, shape: Sequence[int], dtype):
-        return torch.from_numpy(self.as_numpy(shape, dtype))
+    def as_gpu_tensor(self):
+        return torch.as_tensor(self._gpu, device=self.device_name)
 
-    def as_gpu_tensor(self, shape: Sequence[int], dtype):
-        return torch.as_tensor(self.as_cupy(shape, dtype), device=self.device_name)
+    def copy_async_host_to_device(self) -> None:
+        memcpy_async_host_to_device(
+            self._gpu_memory.data,
+            self._cpu_memory.data,
+            self._size,
+            self._stream.ptr,
+        )
 
-    def to_async_device(self):
-        pass
+    def copy_async_device_to_host(self) -> None:
+        memcpy_async_device_to_host(
+            self._cpu_memory.data,
+            self._gpu_memory.data,
+            self._size,
+            self._stream.ptr,
+        )
 
-    def to_async_host(self):
-        pass
+    def record(self) -> None:
+        self._event.record(self._stream)
 
-    def sync(self):
-        pass
+    def synchronize(self):
+        self._stream.synchronize()
--- tester/cuda/test_handler.py
+++ tester/cuda/test_handler.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from functools import reduce
 from unittest import TestCase, main, skipIf
+
+import numpy
 
 from smipc.cuda.alignment import IPC_ALLOCATION_UNIT_SIZE
 from smipc.cuda.compatibility import has_cupy
@@ -11,10 +14,33 @@ from smipc.cuda.handler import CudaHandler
 class HandlerTestCase(TestCase):
     def test_default(self):
         with self.assertRaises(ValueError):
-            CudaHandler(0)
+            CudaHandler((0, 0), dtype=numpy.uint8)
 
-        handler = CudaHandler(1)
-        self.assertEqual(IPC_ALLOCATION_UNIT_SIZE, handler.size)
+        shape = 1920, 1080, 3
+        size = reduce(lambda x, y: x * y, shape, 1)
+        unit = IPC_ALLOCATION_UNIT_SIZE
+        unit2x = unit * 2
+        unit3x = unit * 3
+        self.assertTrue(unit2x < size < unit3x)
+
+        empty = numpy.ones(shape, dtype=numpy.uint8)
+        self.assertEqual(size, empty.size)
+
+        handler = CudaHandler(shape, dtype=numpy.uint8)
+        self.assertEqual(unit3x, handler.size)
+
+        handler.cpu[:] = empty[:]
+        handler.copy_async_host_to_device()
+        handler.record()
+        handler.synchronize()
+
+        handler.gpu *= 10
+
+        handler.copy_async_device_to_host()
+        handler.record()
+        handler.synchronize()
+
+        self.assertTrue(numpy.all(handler.cpu.all == 10))
 
 
 if __name__ == "__main__":
